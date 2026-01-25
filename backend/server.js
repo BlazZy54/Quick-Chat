e307a1e572e5b4a00c9915d0d9633011c3fa54a4
Mayank Singh <iamayanksk@gmail.com>
2026-01-25 13:17:28 +0530

    added the deployment chnages

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import verifycookieRoutes from "./routes/verifycookie.routes.js"
 
-import {server, app} from './socket/socket.js'
+import { server, app } from './socket/socket.js'
 
 // const app = express()
 const PORT = process.env.PORT || 3000
@@ -20,10 +20,24 @@ app.use(express.json()) //to parse incoming requests from JSON payloads and get 
 app.use(cookieParser()) //reads cookies from incoming HTTP requests and makes them available as req.cookies.
 
 //middlewares that handles routes
-app.use('/api/auth',authRoutes)
+app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/verifycookie', verifycookieRoutes)
+
+
+//DEPLOYMENT
+import path from "path"
+const __dirname = path.resolve() //root directory
+
+//express.static to serve static files publically
+app.use(express.static(path.join(__dirname, "/frontend/dist")))  //middleware
+
+if (process.env.NODE_ENV == 'deployment') {
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname, "/frontend/dist/index.html")      //serve the builded frontend files
+    })
+}
 
 
 server.listen(PORT, () => {
