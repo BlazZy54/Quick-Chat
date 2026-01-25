5932c55c80accdd8194b3d6da58095794e64412f
Mayank Singh <iamayanksk@gmail.com>
2026-01-25 15:32:44 +0530

    updated useListenMessages.js

--- frontend/src/Socket/useListenMessages.js
+++ frontend/src/Socket/useListenMessages.js
@@ -8,19 +8,39 @@ const useListenMessages = () => {
     const { messages, setMessages } = conversationStore()
 
     useEffect(() => {
-        // socket.on() is used to listen to the events and can be used both on server side and on client side
-        socket?.on("NewMessage", (new_msg) => {
-            setMessages([...messages, new_msg])
-            new_msg.shouldShake = true
-            const sound = new Audio(noti_sound)
-            sound.volume = 0.005;
-            sound.play()
-        })
+        if (!socket) return;
 
-        //cleanup function
-        return () => socket?.off("NewMessage")  //important
-    }, [socket, setMessages])
+        //chatgpt
+        const handler = (new_msg) => {
+            setMessages((prev) => [...prev, new_msg]);
+            const sound = new Audio(noti_sound);
+            sound.volume = 0.005;
+            sound.play();
+        };
+
+        socket.on("NewMessage", handler);
+
+        return () => socket.off("NewMessage", handler);
+    }, [socket]);
+
 
 }
 
 export default useListenMessages
+
+
+
+//og -> 
+// useEffect(() => {
+//     // socket.on() is used to listen to the events and can be used both on server side and on client side
+//     socket?.on("NewMessage", (new_msg) => {
+//         setMessages([...messages, new_msg])
+//         new_msg.shouldShake = true
+//         const sound = new Audio(noti_sound)
+//         sound.volume = 0.005;
+//         sound.play()
+//     })
+
+//     //cleanup function
+//     return () => socket?.off("NewMessage")  //important
+// }, [socket, setMessages])
